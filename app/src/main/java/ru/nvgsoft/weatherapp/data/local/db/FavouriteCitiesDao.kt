976d26c6dd1052afa472c9254acbf5a5a43a4f55
Daniil Shcherbakov <Charel@bk.ru>
2025-06-26 17:48:59 +0300

    Create Database

---
+++ app/src/main/java/ru/nvgsoft/weatherapp/data/local/db/FavouriteCitiesDao.kt
@@ -0,0 +1,27 @@
+package ru.nvgsoft.weatherapp.data.local.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+import ru.nvgsoft.weatherapp.data.local.model.CityDbModel
+
+@Dao
+interface FavouriteCitiesDao {
+
+    @Query("SELECT * FROM favourite_cities")
+    fun getFavouriteCities(): Flow<CityDbModel>
+
+    @Query("SELECT EXISTS (SELECT * FROM favourite_cities WHERE id=:cityId LIMIT 1)")
+    fun observeIsFavourite(cityId: Int): Flow<Boolean>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addFavourite(cityDbModel: CityDbModel)
+
+    @Query("DELETE FROM favourite_cities WHERE id=:cityId")
+    suspend fun deleteFavourite(cityId: Int)
+
+
+}
