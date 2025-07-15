583c1452e929862f059befb897b912c423fe3b39
Daniil Shcherbakov <Charel@bk.ru>
2025-07-15 19:59:51 +0300

    fix misprint

--- app/src/main/java/ru/nvgsoft/weatherapp/presentation/search/SearchContent.kt
+++ app/src/main/java/ru/nvgsoft/weatherapp/presentation/search/SearchContent.kt
@@ -24,7 +24,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-fiximport androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
